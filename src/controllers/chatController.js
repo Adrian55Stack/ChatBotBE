e2c9c2c1c3abae87b4617af64a9823594fc2def8
Adrian55Stack <adrianciurea112@yahoo.com>
2026-09-05 14:05:38 +0300

    Implement basic translation

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -3,15 +3,17 @@ import {askGroq} from '../utils/askGroq.js';
 
 export const sendMessage = async (req, res) => {
     try {
-    const question = req.body.question;
+    const [question, detected_language] = await translateString(req.body.question, 'EN');
 
     const context = await searchMythology(question);
 
     const answer = await askGroq(context, question);
 
+    const [translatedAnswer] = await translateString(answer, detected_language);
+
     res.json({
       question,
-      answer,
+      translatedAnswer,
       contextUsed: context,
     });
   } catch (err) {
@@ -19,4 +21,20 @@ export const sendMessage = async (req, res) => {
     res.status(500).json({ error: "Chat failed" });
   }
 };
+
+async function translateString(question, target_lang) {
+  const response = await fetch("https://api-free.deepl.com/v2/translate", {
+    method: "POST",
+    headers: {
+      "Authorization": `DeepL-Auth-Key ${process.env.DEEP_L_API_kEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text: [question], target_lang }),
+  });
+
+  const resp = await response.text();
+  const responseJSON = JSON.parse(resp);
+  const responseData = responseJSON.translations[0];
+  return [responseData.text, responseData.detected_source_language];
+}
 
