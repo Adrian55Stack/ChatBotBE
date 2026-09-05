67a7edeb4f0c8ef2d3abbd0efa54ecfdbed9c889
Adrian55Stack <adrianciurea112@yahoo.com>
2026-09-05 14:17:24 +0300

    Refactor method

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,5 +1,6 @@
 import {searchMythology} from '../utils/searchMythology.js';
 import {askGroq} from '../utils/askGroq.js';
+import { translateString } from '../utils/translateString.js';
 
 export const sendMessage = async (req, res) => {
     try {
@@ -21,20 +22,3 @@ export const sendMessage = async (req, res) => {
     res.status(500).json({ error: "Chat failed" });
   }
 };
-
-async function translateString(question, target_lang) {
-  const response = await fetch("https://api-free.deepl.com/v2/translate", {
-    method: "POST",
-    headers: {
-      "Authorization": `DeepL-Auth-Key ${process.env.DEEP_L_API_kEY}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ text: [question], target_lang }),
-  });
-
-  const resp = await response.text();
-  const responseJSON = JSON.parse(resp);
-  const responseData = responseJSON.translations[0];
-  return [responseData.text, responseData.detected_source_language];
-}
-
